64d854591e992ab43e1e05ee43dd53af1d9736c1
Rahul <rahul.es2024@gmail.com>
2024-04-19 01:56:14 +0200

    checking

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           <a class="nav-link" href="/">Features</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/">Pricing</a>
+          {/* <a class="nav-link" href="/">Pricing</a> */}
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
